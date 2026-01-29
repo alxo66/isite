c24591db148df58e179e6d7ec18e1a311f970ce4
alxo66 <alxo6677@gmail.com>
2026-01-30 03:59:04 +0500

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import { CartProvider } from '@/context/CartContext'
+import TelegramWidget from '@/components/TelegramWidget'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,6 +27,7 @@ export default function RootLayout({
             <main className="flex-grow">{children}</main>
             <Footer />
           </div>
+          <TelegramWidget />
         </CartProvider>
       </body>
     </html>
