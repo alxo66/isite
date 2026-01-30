4d25afd8e6ef9d3a0927978d731ee5e62db28a2e
alxo66 <alxo6677@gmail.com>
2026-01-31 02:44:45 +0500

    client-layout.tsx

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -5,19 +5,3 @@ import Footer from '@/components/Footer'
 import { CartProvider } from '@/context/CartContext'
 import TelegramWidget from '@/components/TelegramWidget'
 
-export default function ClientLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <CartProvider>
-      <div className="min-h-screen flex flex-col">
-        <Header />
-        <main className="flex-grow">{children}</main>
-        <Footer />
-        <TelegramWidget />
-      </div>
-    </CartProvider>
-  )
-}
