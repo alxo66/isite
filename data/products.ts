70a278905fc22f3a722668a82fa7ed1b8f6995b3
alxo66 <alxo6677@gmail.com>
2026-01-31 18:56:10 +0500

    products.ts

--- data/products.ts
+++ data/products.ts
@@ -1,5 +1,3 @@
-// data/products.ts
-
 export interface Product {
   id: string
   title: string
@@ -12,28 +10,28 @@ export const products: Product[] = [
   {
     id: 'iphone-16-pro-max',
     title: 'iPhone 16 Pro Max',
-    description: '6.9" OLED, A18 Pro, 1TB, Titanium корпус',
+    description: '6.9″ OLED, A18 Pro, 1TB, Titanium',
     price: 1399,
     image: '/products/iphone-16-pro-max.jpg',
   },
   {
     id: 'iphone-16-pro',
     title: 'iPhone 16 Pro',
-    description: '6.3" OLED, A18 Pro, 512GB',
+    description: '6.3″ OLED, A18 Pro, 512GB',
     price: 1199,
     image: '/products/iphone-16-pro.jpg',
   },
   {
     id: 'iphone-16-plus',
     title: 'iPhone 16 Plus',
-    description: '6.7" OLED, A18, 256GB',
+    description: '6.7″ OLED, A18, 256GB',
     price: 999,
     image: '/products/iphone-16.jpg',
   },
   {
     id: 'iphone-16',
     title: 'iPhone 16',
-    description: '6.1" OLED, A18, 128GB',
+    description: '6.1″ OLED, A18, 128GB',
     price: 899,
     image: '/products/iphone-16.jpg',
   },
